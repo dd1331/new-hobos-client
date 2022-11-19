b05c9bff4553809b1b63a70ba4d7c3811e80c4d6
Chalie Choi <ddd1331@gmail.com>
2022-11-19 18:51:55 +0900

    activate currunt category button

--- src/components/GoodCategory.vue
+++ src/components/GoodCategory.vue
@@ -7,17 +7,28 @@
       rounded="lg"
       variant="flat"
       height="35"
+      :color="category.id === state.categoryId ? 'primary' : 'grey'"
       @click="toCategoryPostList(category.id)"
-      >{{ category.title }}</v-btn
-    >
+      >{{ category.title }}
+    </v-btn>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useCategoryStore } from "@/stores/category";
-import { useRouter } from "vue-router";
+import { onMounted, reactive } from "vue";
+import { onBeforeRouteUpdate, useRoute, useRouter } from "vue-router";
 const router = useRouter();
+const route = useRoute();
+const state = reactive({ categoryId: 0 });
 async function toCategoryPostList(categoryId: number) {
   router.push({ path: "/post", query: { category: categoryId } });
 }
+
+onMounted(async () => {
+  state.categoryId = Number(route.query.category);
+});
+onBeforeRouteUpdate(async (to) => {
+  state.categoryId = Number(to.query.category);
+});
 </script>
